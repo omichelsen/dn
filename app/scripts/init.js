73c989aa2a0c37f8c35f41a73f8a838aa2fed70d
Ole Bjørn Michelsen <ole@michelsen.dk>
2015-09-25 14:13:44 +0200

    remove wrapping

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -1,18 +1,13 @@
-(function (window, undefined) {
-    'use strict';
+// Init fastclick
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function () {
+        FastClick.attach(document.body);
+    }, false);
+}
 
-    // Init fastclick
-    if ('addEventListener' in document) {
-        document.addEventListener('DOMContentLoaded', function () {
-            FastClick.attach(document.body);
-        }, false);
-    }
-
-    // Prevent content scroll on touch devices
-    if ('ontouchstart' in window) {
-        document.getElementById('page-main').addEventListener('touchmove', function (e) {
-            e.preventDefault();
-        });
-    }
-
-}(this));
+// Prevent content scroll on touch devices
+if ('ontouchstart' in window) {
+    document.getElementById('page-main').addEventListener('touchmove', function (e) {
+        e.preventDefault();
+    });
+}
